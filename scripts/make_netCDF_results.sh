fd6692fcd2e217da7aa9b311c9627eda3ce29e40
Rafael Vieira <rafaelvieirac@hotmail.com>
2021-03-04 22:19:57 -0300

    'make_netCDF_results.sh' file is done!

--- scripts/make_netCDF_results.sh
+++ scripts/make_netCDF_results.sh
@@ -4,24 +4,24 @@
 # ******************************* MAKE NETCDF RESULTS ****************************** #
 #																					 #
 # This script runs all the python routines responsible for converting results        #
-# from ".mat" to "netCDF"															 #
+# from ".mat" to "netCDF"   														 #
 # ********************************************************************************** #
 
 
-export workdir=/home/oceano/WW3-v6.07/model/ww3_shell/modelo_hindcast
+export workdir=/home/piatam8/ww3/ww3_shell/modelo_hindcast
 
 cd $workdir/scripts
 
-./cria_netCDF_dir_geral.py
+ipython cria_netCDF_dir_geral.py
 echo " *** NetCDF Wdir file is done! ***"
 
-./cria_netCDF_energyTransp_geral.py
+ipython cria_netCDF_energyTransp_geral.py
 echo " *** NetCDF energyTransp file is done! ***"
 
-./cria_netCDF_hs_geral.py
+ipython cria_netCDF_hs_geral.py
 echo " *** NetCDF Hs file is done! ***"
 
-./cria_netCDF_tp_geral.py
+ipython cria_netCDF_tp_geral.py
 echo " *** NetCDF Tp file is done! ***"
 
 echo " ************* End of Program *************"
