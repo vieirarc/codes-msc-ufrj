0c4260389622527b728cd4966e7ff4f03a19b149
vieirarc <rafaelvieirac@hotmail.com>
2021-03-16 12:03:47 -0300

    small changes in average energy images

--- scripts/scripts_graficos/cria_imagens_energyTransp_swan_monthlyAverage_geral.py
+++ scripts/scripts_graficos/cria_imagens_energyTransp_swan_monthlyAverage_geral.py
@@ -428,9 +428,10 @@ for k, v in averageMonthlyDict.items():
     year = str(k)[7:11]
 
     #print(year,month,day,hour)
-    lvl = np.arange(0, 12100, 300)
-    levels = range(0, 12100, 1000)
-    strs = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+    lvl = np.arange(0, 12100, 2000)
+    levels = range(0, 12100, 2000)
+    #strs = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+    strs = ['0', '2', '4', '6', '8', '10', '12']
     cf = plt.contourf(lon, lat, v, lvl, vmin=0, vmax=12100, shading='gouraud')
     im = plt.contour(lon, lat, v, levels, colors='white', linestyles='solid')
     fmt = {}
@@ -480,6 +481,7 @@ for k, v in averageMonthlyDict.items():
     cbar = fig.colorbar(cf, orientation='vertical', pad=0.025)
     cbar.set_label('Wave Power (kW/m)', size=7, rotation=90, labelpad=3)
     cbar.set_ticks([np.arange(0, 12100, 1000)]) #update_ticks=True
+    cbar.set_tickslabels([np.arange(0, 13, 1)]) #update_ticks=True
     cbar.ax.tick_params(labelsize=6)
     #hour = str(k)[12:18]
     #day = str(k)[9:11]
