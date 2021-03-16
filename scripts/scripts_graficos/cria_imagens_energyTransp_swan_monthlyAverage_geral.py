ab8eb41c8bf975f131bf696d4df963b663236b63
vieirarc <rafaelvieirac@hotmail.com>
2021-03-16 10:59:39 -0300

    small changes in average energy images

--- scripts/scripts_graficos/cria_imagens_energyTransp_swan_monthlyAverage_geral.py
+++ scripts/scripts_graficos/cria_imagens_energyTransp_swan_monthlyAverage_geral.py
@@ -430,13 +430,13 @@ for k, v in averageMonthlyDict.items():
     #print(year,month,day,hour)
     lvl = np.arange(0, 12100, 300)
     levels = range(0, 12100, 1000)
-    strs = ['0', '1.0', '2.0', '3.0', '4.0', '5.0', '6.0', '7.0', '8.0', '9.0', '10.0', '11.0', '12.0']
+    strs = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
     cf = plt.contourf(lon, lat, v, lvl, vmin=0, vmax=12100, shading='gouraud')
     im = plt.contour(lon, lat, v, levels, colors='white', linestyles='solid')
     fmt = {}
     for l, s in zip(im.levels, strs):
         fmt[l] = s
-    isobaths_labels = plt.clabel(im, fmt, colors='white', fontsize=6)
+    isobaths_labels = plt.clabel(im, fmt.values, colors='white', fontsize=6)
     plt.setp(isobaths_labels, path_effects=[PathEffects.withStroke(linewidth=1.2, foreground="k")])
     plt.axis([np.min(lon), np.max(lon), np.min(lat), np.max(lat)])
     #xF, yF  = (-43.20,  -22.93)
